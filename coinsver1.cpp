f847a86ad1ab33577bc1fc31112d1c580b8d6acb
BYOA <34205945+BYOA@users.noreply.github.com>
2017-12-23 19:22:58 +1100

    Create coinsver1.cpp

---
+++ coinsver1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    int answer;
+    cout << "Input coin amount in cents to find the minimum amount of coins to become the equivlant of it.\n cents:";
+    cin >> answer;
+    double copyAnswer = answer;
+
+    vector<int> coins = {200, 100 , 50, 20, 10, 5}; //australian coins in cents. May exclude 1 and 2 dollar coins
+            vector <int> total(6);
+    int overall = 0;
+
+
+    for (int i = 0; i < coins.size() ; i++)
+    {
+        int tally = 0;
+        if (answer >= coins[i])
+        {
+            for (;answer >=coins[i];tally++)
+            {
+                cout << coins[i] << ' ';
+                answer -= coins[i];
+            }
+
+        }
+        /*different if statements to account for numbers that need multiple different types of coins*/
+        if ((answer >=3 && answer <=4) && coins[i] == 5) //accounting for rounding
+           {
+                cout << coins[i] << ' ';
+               tally++;
+           }
+
+        overall += tally;
+        total[i] = tally;
+    }
+
+cout << "\n\n\nThe minimum amount of coins to obtain " << copyAnswer << " cents is: " << overall << endl << endl;
+
+    for (int i = 0; i < coins.size() ; i++)
+    {
+        cout << "\n\n" << total[i] << ' ' << coins[i] << " cent coins.";
+
+    }
+}
