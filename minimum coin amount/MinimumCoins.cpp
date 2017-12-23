f0d1f0c080ef3671e9cb5ac3921e7581f6744f68
BYOA <34205945+BYOA@users.noreply.github.com>
2017-12-23 23:04:43 +1100

    Update MinimumCoins.cpp

--- minimum coin amount/MinimumCoins.cpp
+++ minimum coin amount/MinimumCoins.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm> //for swap
+#include "coin.h"
 
-using namespace std;
-void coinSort (vector<int> &coins,  bool (*comparision)(int, int));
-void displayCoinOrder (vector<int> coinOrder);
-bool ascending (int x, int y);
-int tallyCalculation (vector<int> coins, vector<int> &total, int value);
 
 int main()
 {
